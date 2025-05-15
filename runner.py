d30367bfdbb9cefc04406c203dbaa939ca78e6c6
Alissa <73993488+alissajouljian@users.noreply.github.com>
2025-05-15 22:59:04 +0400

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,10 +1,12 @@
 import subprocess
 
 def compile_and_run(asm_path, bin_path):
-    obj_path = asm_path.replace(".asm", ".o")
+    # Compile the .s file with nasm
+    obj_path = asm_path.replace(".s", ".o")
 
-    subprocess.run(["nasm", "-felf64", asm_path, "-o", obj_path], check=True)
-    subprocess.run(["ld", "-o", bin_path, obj_path], check=True)
-    print("\n--- Program Output ---")
-    subprocess.run([f"./{bin_path}"])
+    subprocess.run(["nasm", "-f", "elf64", asm_path, "-o", obj_path], check=True)
+    # Link with ld
+    subprocess.run(["ld", obj_path, "-o", bin_path], check=True)
+    # Run the compiled binary
+    subprocess.run([f"./{bin_path}"], check=True)
 
